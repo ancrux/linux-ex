93596a855ed6ff357efc84fd2f6ba386371ddcd5
angusliu <angusliu1975@gmail.com>
2015-09-02 10:53:37 +0800

    [add] tar

---
+++ tar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Task: List the contents of a tar file
+tar -tvf file.tar
+
+# Task: List the contents of a tar.gz file
+tar -ztvf file.tar.gz
+
+# Task: List the contents of a tar.bz2 file
+tar -jtvf file.tar.bz2
+
