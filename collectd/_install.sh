420e01ac20fa35dda30054dadb7ea65654862507
angusliu <angusliu1975@gmail.com>
2015-09-17 17:03:07 +0800

    [add] collectd installation comment

--- collectd/_install.sh
+++ collectd/_install.sh
@@ -2,7 +2,10 @@
 
 yum localinstall collectd
 yum localinstall collectd-python
+# collectd configuration:FQDNLookup   false
 
 # copy ./root/* to target machine
+pip install -r /usr/share/collectd/docker/requirements.txt
+# Note: must add TypesDB "/usr/share/collectd/types.db" with TypesDB "/usr/share/collectd/docker/dockerplugin.db" in collectd configuration
 service collectd restart
 
