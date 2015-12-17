f85870dcd559837609a8d4f67adf5c9e56239a98
angusliu <angusliu1975@gmail.com>
2015-12-17 16:11:23 +0800

    [add] patch with relative path

--- patch.sh
+++ patch.sh
@@ -4,6 +4,10 @@
 diff /tmp/t1/a.txt /tmp/t2/b.txt > patch.diff
 patch -p0 < patch.diff
 
+# if you wish to generate relative path, change working directory and execute patch command in the working directory
+cd /tmp/t1; \
+  diff a.txt /tmp/t2/b.txt > patch.diff
+
 # patch -p
 # -p0 patching file with full path (e.g. /tmp/t1/a.txt => /tmp/t1/a.txt)
 # -p1 take one slash out (e.g. /tmp/t1/a.txt => tmp/t1/a.txt)
