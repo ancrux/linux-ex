c06175186c6c001fce2582ff20ddfdcbc63459f0
angusliu <angusliu1975@gmail.com>
2015-12-17 11:57:25 +0800

    [add] diff two folders but exclude a sub folder

--- diff.sh
+++ diff.sh
@@ -3,6 +3,9 @@
 # diff two folders
 diff -bur folder1/ folder2/
 
+# diff two folders but exclude a folder
+diff --exclude=.git -bur /git-rf8/saas_ansible_deploy /git-rf5/saas_ansible_deploy
+
 # diff two remote files
 diff <(ssh myServer1 'cat foo') <(ssh myServer2 'cat foo') 
 
