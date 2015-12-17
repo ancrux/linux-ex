4690be8ffb3f936f2ba471ef3f532ac4e83bcb4e
angusliu <angusliu1975@gmail.com>
2015-12-17 12:03:40 +0800

    [add] git diff exclude pattern

--- diff.sh
+++ diff.sh
@@ -5,6 +5,7 @@ diff -bur folder1/ folder2/
 
 # diff two folders but exclude a folder
 diff --exclude=.git -bur /git-rf8/saas_ansible_deploy /git-rf5/saas_ansible_deploy
+diff --exclude=.git* -bur /git-rf8/saas_ansible_deploy /git-rf5/saas_ansible_deploy # skip .gitignore, too
 
 # diff two remote files
 diff <(ssh myServer1 'cat foo') <(ssh myServer2 'cat foo') 
