d04a057655ceab627605af19fd2396d943cac213
angusliu <angusliu1975@gmail.com>
2015-12-17 11:23:18 +0800

    [add] colordiff example

--- diff.sh
+++ diff.sh
@@ -6,3 +6,7 @@ diff -bur folder1/ folder2/
 # diff two remote files
 diff <(ssh myServer1 'cat foo') <(ssh myServer2 'cat foo') 
 
+# colorize diff output
+yum install -y colordiff
+diff a.txt b.txt | colordiff
+
