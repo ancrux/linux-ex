ab418beacac29ff6f96d9bc55d11a867512c84cb
angusliu <angusliu1975@gmail.com>
2015-12-25 11:36:38 +0800

    [add] yum deplist

--- yum.sh
+++ yum.sh
@@ -10,6 +10,12 @@ yumdownloader <package> || yum install yum-downloadonly && yum update <package> 
 yum provides "*/scp"
 yum provides httpd
 
+# get dependencies of non-installed package
+yum deplist php-silex # this will full list (maybe hundreds of dependencies)
+# use repoquery from yum-utils
+repoquery --requires php-silex # this only return requires in rpm.spec
+repoquery --requires --resolve php-silex # this return names of rpm
+
 # daemon for increasing yum performance
 yum install yum-cron
 service yum-cron restart
