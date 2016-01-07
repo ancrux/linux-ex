e8c12b88e61365d46973d6bfde681a95aeda9ed7
angusliu <angusliu1975@gmail.com>
2016-01-07 11:08:35 +0800

    [add] yum example

--- yum.sh
+++ yum.sh
@@ -9,6 +9,12 @@ yumdownloader <package> || yum install yum-downloadonly && yum update <package> 
 # yumdownloader along with dependencies
 yumdownloader --resolve <package>
 
+# install from local packages and dependencies
+yum localinstall *.rpm
+
+# An /etc/yum.conf with the appropriate URL's in the config file. This could point at http://localhost/SOFTWARE/ or file:///usr/local/SOFTWARE/
+# in the SOFTWARE directory, you will need headers. What linux version are you running? For newer software, createrepo seems to be the key. I think the command "yum-arch ." builds the headers too.
+
 # search a package with filename
 yum provides "*/scp"
 yum provides httpd
